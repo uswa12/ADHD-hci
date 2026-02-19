d99d1f7cd92da74e4e05ef02d86da584666a0272
Uswa Khan <154889180+uswa12@users.noreply.github.com>
2026-02-19 21:14:26 +0500

    Delete start.sh

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# start.sh at root to run app.py inside focusflow
-
-# Install dependencies (optional, Railpack may already do this)
-pip install -r requirements.txt
-
-# Run the app
-python focusflow/app.py
